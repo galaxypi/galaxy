df1405bc2dd47604624080e2ecc963d263385a4d
Jacob Gadikian <jacobgadikian@gmail.com>
2018-07-09 18:34:53 -0400

    Update install-mac.sh

--- install-mac.sh
+++ install-mac.sh
@@ -4,17 +4,29 @@
 # * wget     | You can "brew install wget"
 
 # puts the daemon and CLI into ~/galaxy, which is on the PATH
+echo "DOWNLOADING DAEMON AND CLI"
 wget -O /usr/local/bin/basecoind https://github.com/galaxypi/galaxy/releases/download/second/basecoind_mac 
 wget -O /usr/local/bin/basecli https://github.com/galaxypi/galaxy/releases/download/second/basecli_mac 
+
+
+echo "INITALIZING BLOCKCHAIN"
+sleep 10
 
 # initalizes the blockchain
 basecoind init
 
+sleep 10
+
+echo "FETCHING GENESIS BLOCK"
 # fetches genesis.json
 wget -O ~/.basecoind/config/genesis.json https://github.com/galaxypi/galaxy/raw/master/genesis.json
 
+sleep 10
+
+
+echo "
 # find-and-replace on config.toml to set seed node
-replace_string='seeds = "c4fc01b8bffd197a0badcc2370641d665c2df6c3@163.172.161.82:26656"'
+replace_string='seeds = "fa2dfb4969ac84726b8969781cf06fdacb979678@98.118.185.162:26656"'
 sed -i -e "s/seeds =/$replace_string/g" ~/.basecoind/config/config.toml
 
 # sync the blockchain
