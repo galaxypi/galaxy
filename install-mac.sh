9c747d4749bce84e6fdc8c415e9b3c090c8c35d7
Guy Lepage <guylepage3@gmail.com>
2018-08-28 19:01:28 -0400

    added spacing mac install #18
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- install-mac.sh
+++ install-mac.sh
@@ -72,7 +72,7 @@ sed -i -e "s/$moniker_original/$moniker_actual/g" "$HOME/.galaxyd/config/config.
 
 # summary
 echo -e "\n\nCongratulations! \xF0\x9F\x8E\x89 \xF0\x9F\x8C\x8C \n
-Galaxy blockchain is now installed and ready to sync......
+Galaxy blockchain is now installed and ready to sync......\n
 
 Navigate into the galaxy directory by typing the following;
 
