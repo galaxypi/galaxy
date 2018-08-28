b7652aa3e4505464d069786f0eb98833caf65f85
Guy Lepage <guylepage3@gmail.com>
2018-08-28 18:52:12 -0400

    udpate to error #18
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- install-mac.sh
+++ install-mac.sh
@@ -79,8 +79,8 @@ Navigate into the galaxy directory by typing the following;\n
 cd ~/galaxy
 "
 
-echo -e "Start sync your Galaxy Node by typing...
-\`~/galaxy\`
-\`./galaxyd start\`
+echo -e "Start sync your Galaxy Node by typing....
+~/galaxy
+./galaxyd start
 
 Note: Syncing your Galaxy Node can take a while."
